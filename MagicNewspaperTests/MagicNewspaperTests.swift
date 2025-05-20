988cce2b792abab11fe7fce30bcabea96ce2c98a
jscheth <sovichea.chet@gmail.com>
2025-05-19 17:01:50 -0700

    Initial Commit

---
+++ MagicNewspaperTests/MagicNewspaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MagicNewspaperTests.swift
+//  MagicNewspaperTests
+//
+//  Created by Jonathan Cheth on 5/19/25.
+//
+
+import Testing
+@testable import MagicNewspaper
+
+struct MagicNewspaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
